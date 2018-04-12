533890a12c09eef60311366c33ad02053e37fdf0
Marina de Lara <lara.muller@workmail.com>
2018-04-12 20:29:55 -0300

    changing view on button click

--- app/src/main/java/com/example/marinadelara/a2048/MainActivity.java
+++ app/src/main/java/com/example/marinadelara/a2048/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.marinadelara.a2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_game);
+            }
+        });
     }
 }
