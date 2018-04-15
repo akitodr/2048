91251fa811cba7ea67c1d50fa951c9902c706672
Marina de Lara <lara.muller@workmail.com>
2018-04-15 02:46:20 -0300

    reset work, revert and score still don't

--- app/src/main/java/com/example/marinadelara/a2048/GameActivity.java
+++ app/src/main/java/com/example/marinadelara/a2048/GameActivity.java
@@ -1,29 +1,59 @@
 package com.example.marinadelara.a2048;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.marinadelara.a2048.Utils.OnSwipeListener;
 import com.example.marinadelara.a2048.customViews.GameBoard;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class GameActivity extends AppCompatActivity {
 
     GestureDetector gestureDetector;
-
     GameBoard gameBoard;
-
+    ImageButton revertButton, resetButton;
+    TextView scoreText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
         gestureDetector = new GestureDetector(this, new SwipeListener());
-
         gameBoard = findViewById(R.id.gameBoard);
-    }
 
+        //BUtton
+        revertButton = findViewById(R.id.revertButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameBoard.reset();
+            }
+        });
+
+        revertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameBoard.movementBeforeCurrent();
+            }
+        });
+        //--end Button
+
+        //Score
+        scoreText = findViewById(R.id.scoreText);
+        System.out.println(gameBoard.getTotalScore());
+        scoreText.append(" " + String.valueOf(gameBoard.getTotalScore()));
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
